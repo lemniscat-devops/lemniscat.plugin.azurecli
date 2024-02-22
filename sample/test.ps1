04903315ab72f4c64435e6e8c94c73082c3dbe3e
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2024-02-22 10:14:13 +0100

    test: update test.ps1

--- sample/test.ps1
+++ sample/test.ps1
@@ -2,4 +2,10 @@ param(
     [string]$name
 )
 
+$result = Get-Location
+
+$variables = Get-Content "$($result.path)/vars.json" | ConvertFrom-Json
+
+Write-Host "Hello, $($variables.rg_name)"
+
 Write-Host "Hello, $name"
