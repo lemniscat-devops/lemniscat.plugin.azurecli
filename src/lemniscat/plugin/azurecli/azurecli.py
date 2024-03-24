ebed52255a45dddc69b358409e205151e4ac2580
pmorisseau <philippe.morisseau@outlook.com>
2024-03-24 08:48:00 +0100

    fix: thread for stdout and stderr (#4)
    * fix: threads for stdout and stderr
    
    * fix: add logs

--- src/lemniscat/plugin/azurecli/azurecli.py
+++ src/lemniscat/plugin/azurecli/azurecli.py
@@ -3,7 +3,9 @@
 
 import logging
 import os
-import subprocess, sys   
+import subprocess, sys
+from queue import Queue
+import threading     
 from lemniscat.core.util.helpers import LogUtil
 from lemniscat.core.model.models import VariableValue
 import re
@@ -15,6 +17,14 @@ except ImportError:
         def emit(self, record):
             pass
 
+def enqueue_stream(stream, queue, type):
+    for line in iter(stream.readline, b''):
+        queue.put(str(type) + line.decode('utf-8').rstrip('\r\n'))
+
+def enqueue_process(process, queue):
+    process.wait()
+    queue.put('x')
+
 logging.setLoggerClass(LogUtil)
 log = logging.getLogger(__name__.replace('lemniscat.', ''))
 
@@ -22,30 +32,35 @@ class AzureCli:
     def __init__(self):
         pass
     
-    def cmd(self, cmds, isMultiline, **kwargs):
+    def cmd(self, cmds, **kwargs):
         outputVar = {}
         capture_output = kwargs.pop('capture_output', True)
-        is_env_vars_included = kwargs.pop('is_env_vars_included', False)
-        if capture_output is True:
-            stderr = subprocess.PIPE
-            stdout = subprocess.PIPE
-        else:
-            stderr = sys.stderr
-            stdout = sys.stdout
-            
-        environ_vars = {}
-        if is_env_vars_included:
-            environ_vars = os.environ.copy()
+        stderr = subprocess.PIPE
+        stdout = subprocess.PIPE
 
         p = subprocess.Popen(cmds, stdout=stdout, stderr=stderr,
                              cwd=None)
         
-        while p.poll() is None:
-            if(isMultiline is True):
-                lines = p.stdout.readlines()
-                errors = p.stderr.readlines()
-                for line in lines:
-                    ltrace = line.decode('utf-8').rstrip('\r\n')
+        q = Queue()
+        to = threading.Thread(target=enqueue_stream, args=(p.stdout, q, 1))
+        te = threading.Thread(target=enqueue_stream, args=(p.stderr, q, 2))
+        tp = threading.Thread(target=enqueue_process, args=(p, q))
+        te.start()
+        to.start()
+        tp.start()
+        
+        if(capture_output is True):
+            while True:        
+                line = q.get()
+                if line[0] == 'x':
+                    break
+                if line[0] == '2':  # stderr
+                    if(line[1:].startswith("ERROR:")):
+                        log.error(f'  {line[1:]}')
+                    else:
+                        log.warning(f'  {line[1:]}')
+                if line[0] == '1':
+                    ltrace = line[1:]
                     m = re.match(r"^\[lemniscat\.pushvar(?P<secret>\.secret)?\] (?P<key>\w+)=(?P<value>.*)", str(ltrace))
                     if(not m is None):
                         if(m.group('secret') == '.secret'):
@@ -54,28 +69,11 @@ class AzureCli:
                             outputVar[m.group('key').strip()] = VariableValue(m.group('value').strip())
                     else:
                         log.info(f'  {ltrace}')
-                for error in errors:
-                    ltrace = error.decode("utf-8").rstrip("\r\n")
-                    if(ltrace.startswith("ERROR:")):
-                        log.error(f'  {ltrace}')
-                    else:
-                        log.warning(f'  {ltrace}')
-            else:
-                line = p.stdout.readline()
-                error = p.stderr.readline()
-                if(line != b''):
-                    ltrace = line.decode('utf-8').rstrip('\r\n')
-                    m = re.match(r"^\[lemniscat\.pushvar(?P<secret>\.secret)\] (?P<key>\w+)=(?P<value>.*)", str(ltrace))
-                    if(not m is None):
-                        if(m.group('secret') == '.secret'):
-                            outputVar[m.group('key').strip()] = VariableValue(m.group('value').strip(), True)
-                        else:
-                            outputVar[m.group('key').strip()] = VariableValue(m.group('value').strip())
-                    else:
-                        log.info(f'  {ltrace}')
-                if(error != b''):
-                    etrace = error.decode("utf-8").rstrip("\r\n")
-                    log.error(f'  {etrace}')    
+
+        tp.join()
+        to.join()
+        te.join()
+             
         out, err = p.communicate()
         ret_code = p.returncode
 
@@ -89,21 +87,23 @@ class AzureCli:
         return ret_code, out, err, outputVar
     
     def append_loginCommand(self, type):
-        self.cmd([type, '-Command', "az config unset core.allow_broker"], True)
-        self.cmd([type, '-Command', "az config set extension.use_dynamic_install=yes_without_prompt"], True)
-        self.cmd([type, '-Command', f"az login --service-principal -u {os.environ['ARM_CLIENT_ID']} -p {os.environ['ARM_CLIENT_SECRET']} --tenant {os.environ['ARM_TENANT_ID']}"], True)
-        self.cmd([type, '-Command', f"az account set --subscription {os.environ['ARM_SUBSCRIPTION_ID']}"], True)
+        log.info("Logging to Azure...")
+        self.cmd([type, '-Command', "az config unset core.allow_broker"], capture_output=False)
+        self.cmd([type, '-Command', "az config set extension.use_dynamic_install=yes_without_prompt"], capture_output=False)
+        self.cmd([type, '-Command', f"az login --service-principal -u {os.environ['ARM_CLIENT_ID']} -p {os.environ['ARM_CLIENT_SECRET']} --tenant {os.environ['ARM_TENANT_ID']}"], capture_output=False)
+        self.cmd([type, '-Command', f"az account set --subscription {os.environ['ARM_SUBSCRIPTION_ID']}"], capture_output=False)
+        log.info("Logged to Azure.")
         
     def run(self, type, command):
         self.append_loginCommand(type)
-        return self.cmd([type, '-Command', command], True)
+        return self.cmd([type, '-Command', command])
 
     def run_script(self, type, script):
         self.append_loginCommand(type)
-        return self.cmd([type, "-f", script], True)
+        return self.cmd([type, "-f", script])
 
     def run_script_with_args(self, type, script, args):
         self.append_loginCommand(type)
         command = [type, "-f", script]
         command.extend(args)
-        return self.cmd(command, True)
+        return self.cmd(command)
